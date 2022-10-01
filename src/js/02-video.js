61cfeff5879c576c5f4c95d58ae713728b7bb3ba
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-10-02 00:58:08 +0300

    task 2 completed, pause, save the current video playback time to local storage,  restart from the same time. update ( throttle) -  1500ms

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+import throttle from "lodash.throttle"; 
+
+ 
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+player.on(`timeupdate`, throttle(onPlay, 1500));
+
+function onPlay({ sec }) {
+    localStorage.setItem("videoplayer-current-time", sec);
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+     
